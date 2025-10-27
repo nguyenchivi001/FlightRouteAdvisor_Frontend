d3f7ef860c9e85e611107113c0e93c31bbcabde6
nguyenchivi001 <nguyenchivi292004@gmail.com>
2025-10-27 14:41:12 +0700

    Edit main.js

--- src/main.js
+++ src/main.js
@@ -1,23 +1,14 @@
-/**
- * main.js
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
-// Plugins
-import { registerPlugins } from '@/plugins'
-
-// Components
-import App from './App.vue'
-
-// Composables
 import { createApp } from 'vue'
-
-// Styles
-import 'unfonts.css'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
+import 'leaflet/dist/leaflet.css'
 
 const app = createApp(App)
 
-registerPlugins(app)
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
